e942d24092a28c11b8fa372e3a10d96dd0c3e2d6
Qendolin <32160662+Qendolin@users.noreply.github.com>
2025-04-04 02:42:14 +0200

    Fix clouds settings mixin

--- src/main/java/com/qendolin/betterclouds/mixin/required/CloudSettingMixin.java
+++ src/main/java/com/qendolin/betterclouds/mixin/required/CloudSettingMixin.java
@@ -1,6 +1,7 @@
 package com.qendolin.betterclouds.mixin.required;
 
-import com.llamalad7.mixinextras.injector.ModifyReturnValue;
+import com.llamalad7.mixinextras.injector.wrapmethod.WrapMethod;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.qendolin.betterclouds.BetterClouds;
 import com.qendolin.betterclouds.config.ConfigManager;
 import net.minecraft.client.option.CloudRenderMode;
@@ -9,7 +10,6 @@ import net.minecraft.client.option.SimpleOption;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.injection.At;
 
 // Run before Iris (priority 1010)
 @Mixin(value = GameOptions.class, priority = 1000)
@@ -19,13 +19,11 @@ public abstract class CloudSettingMixin {
     private SimpleOption<CloudRenderMode> cloudRenderMode;
 
     @SuppressWarnings("UnresolvedMixinReference")
-    @ModifyReturnValue(method = {"getCloudRenderModeValue", "getCloudsType", "method_1632", "m_92174_"}, remap = false, at = @At("RETURN"))
-    private CloudRenderMode overrideCloudRenderMode(CloudRenderMode value) {
-        if (!BetterClouds.isEnabled())
-            return value;
-        if (ConfigManager.instance().cloudOverride) {
-            value = cloudRenderMode.getValue();
+    @WrapMethod(method = {"getCloudRenderModeValue", "getCloudsType", "method_1632", "m_92174_"}, remap = false)
+    private CloudRenderMode overrideCloudRenderMode(Operation<CloudRenderMode> original) {
+        if (BetterClouds.isEnabled() && ConfigManager.instance().cloudOverride) {
+            return cloudRenderMode.getValue();
         }
-        return value;
+        return original.call();
     }
 }
