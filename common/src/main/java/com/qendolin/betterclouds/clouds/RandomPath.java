dc86396d435c4121eafbe75018fc5701e09695e6
orangishcat <fdsnail2@gmail.com>
2026-04-29 22:11:57 -0700

    fix: fix this too just in case (currently not used)

--- common/src/main/java/com/qendolin/betterclouds/clouds/RandomPath.java
+++ common/src/main/java/com/qendolin/betterclouds/clouds/RandomPath.java
@@ -14,7 +14,7 @@ public class RandomPath {
     private static int pathWrap;
 
     private static double getPathLinear(double time, double travelSpeed, int coordinateIndex) {
-        double x = time / TICKS_PER_POINT * travelSpeed;
+        double x = Math.max(0, time) / TICKS_PER_POINT * travelSpeed;
         int index = Mth.floor(x);
         double fractionalPart = Mth.frac(x);
 
