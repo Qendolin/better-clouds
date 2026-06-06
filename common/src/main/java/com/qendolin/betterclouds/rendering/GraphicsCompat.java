fa6227f26957fcabfd2e1ae0239700039c201171
orangishcat <fdsnail2@gmail.com>
2026-06-05 21:08:31 -0700

    fix: force renderer to blaze3d if using vulkan

--- common/src/main/java/com/qendolin/betterclouds/rendering/GraphicsCompat.java
+++ common/src/main/java/com/qendolin/betterclouds/rendering/GraphicsCompat.java
@@ -4,12 +4,20 @@ import com.qendolin.betterclouds.BetterCloudsStatic;
 import com.qendolin.betterclouds.compat.GLCompat;
 import com.qendolin.betterclouds.config.Config;
 import com.qendolin.betterclouds.config.ConfigManager;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.PreferredGraphicsApi;
 
 public abstract class GraphicsCompat {
     public static GraphicsCompat instance;
     public static boolean isOpenGL = false;
 
     public static void initCompat() {
+        if (ConfigManager.instance().renderer == Config.Renderer.OPENGL &&
+                PreferredGraphicsApi.VULKAN.equals(Minecraft.getInstance().options.preferredGraphicsBackend().get())) {
+            ConfigManager.instance().renderer = Config.Renderer.BLAZE3D;
+            BetterCloudsStatic.getLogger().warn("Preferred graphics backend is Vulkan, forcing renderer to Blaze3D");
+        }
+
         isOpenGL = ConfigManager.instance().renderer == Config.Renderer.OPENGL;
         if (isOpenGL)
             instance = new GLCompat(BetterCloudsStatic.IS_DEV);
