bd7ad08b41c9165f3da5aae6d295d2296c4ffbf5
orangishcat <fdsnail2@gmail.com>
2026-07-10 10:25:59 -0700

    fix: change publishing order so that 1.21.11-fabric is last

--- publish-and-switch.sh
+++ publish-and-switch.sh
@@ -1,12 +1,12 @@
 set -euo pipefail
 
 versions=(
-  "1.21.11-fabric"
-  "1.21.11-forge"
-  "1.21.1-fabric"
-  "1.21.1-forge"
-  "1.20.6-fabric"
   "1.20.6-forge"
+  "1.20.6-fabric"
+  "1.21.1-forge"
+  "1.21.1-fabric"
+  "1.21.11-forge"
+  "1.21.11-fabric"
 )
 
 for ver in "${versions[@]}"; do
